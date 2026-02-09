faac1d5a23bc1b2794407a2adb2b309006c20761
Akash Kulshrestha (IT Consultant) <akulshrestha@darglobal.co.uk>
2026-02-09 19:30:47 +0530

    first Assigment

--- Ass1/ASSIGNMENT_1_Task 2.py
+++ Ass1/ASSIGNMENT_1_Task 2.py
@@ -1,4 +1,4 @@
 first = input("Please your first name: ")
 last = input("Please your last name: ")
 
-print("Hello", first, last+'! Welcome to Python Program')
+print("Hello", first, last+'! Welcome to Python Program.')
