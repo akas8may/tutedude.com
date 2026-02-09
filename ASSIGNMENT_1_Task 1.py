5e0516fb05a9b2410966ecce816178ee2e090220
Akash Kulshrestha (IT Consultant) <akulshrestha@darglobal.co.uk>
2026-02-09 18:24:11 +0530

    first Assigment

---
+++ ASSIGNMENT_1_Task 1.py
@@ -0,0 +1,7 @@
+first= int(input("Please enter first number: "))
+second= int(input("Please enter second number: "))
+
+print("Addition",first+second)
+print("Subtraction",first-second)
+print("Multiplication",first*second)
+print("Division",first/second)
---
+++ ASSIGNMENT_1_Task 2.py
@@ -0,0 +1,4 @@
+first = input("Please your first name: ")
+last = input("Please your last name: ")
+
+print("Hello", first, last+'! Welcome to Python Program.')
